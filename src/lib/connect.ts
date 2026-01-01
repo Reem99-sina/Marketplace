4a336d9840bb850a215f5c2d3875bae2e1ca3d16
Reem99-sina <reemsina2@gmail.com>
2026-01-01 23:03:24 +0200

    Marketplace system

--- src/lib/connect.ts
+++ src/lib/connect.ts
@@ -8,7 +8,7 @@ const connect = async () => {
 
   return await  mongoose
     .connect(
-      process.env.DATABASE_URL! 
+      process.env.DATABASE_URL||'mongodb+srv://reemsina:A6dg7ia4%40@cluster0.f01jr9o.mongodb.net/marketplace'
     )
     .then(() => {
       console.log("done connect to database");
